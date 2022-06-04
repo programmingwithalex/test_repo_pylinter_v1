44da8f8dc8c573ae94eab85ce29df2cbf4554c11
programmingwithalex <5667711+programmingwithalex@users.noreply.github.com>
2022-06-04 18:52:25 -0400

    adding back linting errors

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
 import os
+import math
 
-
-def func1(a, b) -> int:
+def func1(a, b) -> str:
     return math.floor(a + b)
 
-
-def func2(a, b, c) -> str:
+def func2(a,b, c) -> int:
     return os.getcwd()
